82a8be86a763c2b8f4184fff4ae2ca40578aee4f
Cali Knowlden <calbearnc@gmail.com>
2024-01-01 18:17:40 -0600

    successful pull of dummy calcuations object in postman GET

--- server/routes/calculator-router.js
+++ server/routes/calculator-router.js
@@ -7,16 +7,16 @@ router.get('/', (req, res) => {
     res.send(calculations);
 });
 
-// router.post('/', (req, res) => {
-//     const newItem = req.body;
+router.post('/', (req, res) => {
+    const newItem = req.body;
   
-//     // create new ID
-//     const lastId = INVENTORY[INVENTORY.length - 1].id;
-//     // add new ID to the movie data
-//     newItem.id = lastId + 1;
+    // create new ID
+    const lastId = INVENTORY[INVENTORY.length - 1].id;
+    // add new ID to the movie data
+    newItem.id = lastId + 1;
   
-//     INVENTORY.push(newItem);
-//     res.sendStatus(200);
-//   });
+    INVENTORY.push(newItem);
+    res.sendStatus(200);
+  });
 
 module.exports = router;
